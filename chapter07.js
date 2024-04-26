586131a9902be5b4414a7da7058276075c682f13
master <ted3131@diquest.com>
2024-04-26 17:08:08 +0900

    commit

---
+++ chapter07.js
@@ -0,0 +1,35 @@
+//1. 대입 연산자
+let var1 = 1;
+
+//2. 산술 연산자
+let num1 = 3 + 2;
+let num2 = 3 - 2;
+
+let num6 = 1 + 2 * 10;
+console.log(num6);
+
+//3.
+let num7 = 10;
+num7 += 20;
+num7 *= 20;
+
+//4.증감 연산자
+let num8 = 10;
+console.log(num8++);
+
+//5.논리 연산자
+let or = true || false;
+
+let and = true && false;
+let not = !true;
+
+console.log(or, and, not);
+
+//6. 비교 연산자
+let comp1 = 1 === "1";
+let comp2 = 1 !== 2;
+console.log(comp1, comp2);
+
+let comp3 = 2 > 1;
+let comp4 = 2 < 1;
+console.log(comp3, comp4);
---
+++ chapter09.js
@@ -0,0 +1,34 @@
+//1.if 조건문
+let num = 4;
+
+if (num >= 10) {
+  console.log("num은 10 이상입니다");
+} else if (num >= 5) {
+  console.log("num 5이상입니다.");
+} else {
+  console.log("거짓입니다.");
+}
+
+//2. switch 문
+//if 문과 기능 자체는 동일
+
+let animal = "cat";
+
+switch (animal) {
+  case "cat": {
+    console.log("고양이");
+    break;
+  }
+  case "dog": {
+    confirm.log("개");
+    break;
+  }
+  case "tiger": {
+    console.log("호랑이");
+    break;
+  }
+
+  default: {
+    console.log("그런 동물은 전 모릅니다.");
+  }
+}
