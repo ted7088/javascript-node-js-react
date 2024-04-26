a80682308eb6c954273b9a1070c7b1e23b62d889
master <ted3131@diquest.com>
2024-04-26 16:16:27 +0900

    commit

---
+++ chapter05.js
@@ -0,0 +1,31 @@
+//1.Number Type;
+let num1 = 27;
+let num2 = 1.5;
+let num3 = -20;
+
+let inf = Infinity;
+letmInf = -Infinity;
+
+let nan = NaN;
+
+//String type
+let myName = "이정환";
+let myLocation = "묵동";
+let introduce = myLocation + myName;
+
+console.log(introduce);
+
+let introductText = `${myName}은 ${myLocation}에 거주합니다.`;
+
+console.log(introductText);
+
+//3. boolean type
+let isSwitchOn = true;
+let isEmpty = false;
+
+//4.Null Type(아무것도 없다)
+let empty = null;
+
+// 5.Undefined Type
+let none;
+console.log(none);
