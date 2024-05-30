425e28a911a7b401806a28c0531fa45156d30a24
master <ted3131@diquest.com>
2024-05-30 16:17:34 +0900

    commit

--- chapter13.js
+++ chapter13.js
@@ -12,27 +12,20 @@ main(sub);
 
 //2.콜백함수의 활용 및 정리
 
-function repeat(count) {
-  for (let idx = 1; idx <= count; idx++) {
-    console.log(idx);
-  }
-}
-
-function repeatDouble(count) {
-  for (let idx = 1; idx <= count; idx++) {
-    console.log(idx * 2);
-  }
-}
-
-repeat(5);
-repeatDouble(5);
-
-function repeatcallbackfunc(count, callback) {
+function repeat(count, callback) {
   for (let idx = 1; idx <= count; idx++) {
     callback(idx);
   }
 }
 
-repeatcallbackfunc(5, function (idx) {
+repeat(5, (idx) => {
   console.log(idx);
+});
+
+repeat(5, (idx) => {
+  console.log(idx * 2);
+});
+
+repeat(5, (idx) => {
+  console.log(idx * 3);
 });
