c9f2d5359590eb15a79121474fa881ca72040217
master <ted3131@diquest.com>
2024-06-12 13:40:53 +0900

    commit

--- section2/chapter04.js
+++ section2/chapter04.js
@@ -5,3 +5,29 @@ let arr1 = [1, 2, 3];
 let arr2 = [4, ...arr1, 5, 6];
 
 console.log(arr2);
+
+let obj1 = {
+  a: 1,
+  a: 2,
+};
+
+let obj2 = {
+  ...obj1,
+  c: 3,
+  c: 4,
+};
+
+function funcA(p1, p2, p3) {
+  console.log(p1, p2, p3);
+}
+
+funcA(...arr1);
+
+//2.Rest 매개변수
+//res는 나머지
+
+function funcB(...rest) {
+  console.log(rest);
+}
+
+funcB(...arr1);
