550a8d3349443aa0caccea2f11d18ca9a764a622
master <ted3131@diquest.com>
2024-06-17 14:05:01 +0900

    commit

--- section2/chapter12.js
+++ section2/chapter12.js
@@ -1,6 +1,6 @@
 function add(a, b, callback) {
   setTimeout(() => {
-    const sum = a + b;
+    const sum = a + b; //3
 
     callback(sum);
   });
@@ -10,13 +10,25 @@ add(1, 2, (value) => {
   console.log(value);
 });
 
-//음식을 주문하는 상황
-function orderFood() {
+// 음식을 주문하는 상황
+function orderFood(callback) {
   setTimeout(() => {
-    const food = "짜장";
+    const food = "짜장면";
+    callback(food); // 콜백 함수로 food 전달
   }, 3000);
 }
 
+function cooldownFood(food, callback) {
+  setTimeout(() => {
+    const cooldownedFood = `식은 ${food}`; // 백틱 사용
+    callback(cooldownedFood);
+  }, 2000);
+}
+
 orderFood((food) => {
-  console.log(food);
+  console.log(food); // "짜장" 출력
+
+  cooldownFood(food, (cooldownedFood) => {
+    console.log(cooldownedFood); // "식은 짜장" 출력
+  });
 });
