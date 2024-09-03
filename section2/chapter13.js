6000bd6081bb846678254c09505fae1d13181243
master <ted3131@diquest.com>
2024-09-03 17:02:17 +0900

    commit

--- section2/chapter13.js
+++ section2/chapter13.js
@@ -1,27 +1,24 @@
-function add10(num) {
-  const promise = new Promise((resolve, reject) => {
-    //비동기 작업
-    //executor
+const promise = new Promise((resolve, reject) => {
+  //비동기 작업 실행하는 함수
+  //executor
 
-    setTimeout(() => {
-      if (typeof num == "number") {
-        resolve(num + 10);
-      } else {
-        reject("num은 숫자 아님");
-      }
-    }, 2000);
-  });
+  setTimeout(() => {
+    const num = 10;
+    if (typeof num === "number") {
+      resolve(num + 10);
+    } else {
+      reject("num 숫자가 아닙니다.");
+    }
+  }, 2000);
+});
 
-  return promise;
-}
+setTimeout(() => {
+  console.log(promise);
+}, 3000);
 
-const p = add10(0);
+//then 메서드
+//->그 후에
 
-p.then((result) => {
-  console.log(result);
-
-  const newP = add10(result);
-  newP.then((result) => {
-    console.log(result);
-  });
+promise.then((value) => {
+  console.log(value);
 });
