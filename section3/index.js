58e1851c73c20b16c9b23514e3c1c168b9d84f2e
master <ted3131@diquest.com>
2024-09-10 11:05:09 +0900

    commit

---
+++ section3/index.js
@@ -0,0 +1,7 @@
+// const moduleData = require("./math");
+import mul from "./math.js";
+import { add, sub } from "./math.js";
+
+console.log(add(1, 2));
+console.log(sub(1, 2));
+console.log(mul(2, 3));
