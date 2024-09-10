91e4161e28c9b9887c315ddf3e4776db6930d968
master <ted3131@diquest.com>
2024-09-10 14:50:34 +0900

    commit

--- section3/index.js
+++ section3/index.js
@@ -2,6 +2,11 @@
 import mul from "./math.js";
 import { add, sub } from "./math.js";
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(mul(2, 3));
+import randomColor from "randomcolor";
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(2, 3));
+
+const color = randomColor();
+console.log(color);
